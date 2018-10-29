c0431ec495a2ea54a8d6a97a3385f8f1c025e097
Justin Davis <justin_w_davis@yahoo.com>
2018-10-29 04:30:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const getFirstSelector = selector => document.querySelector( selector )
 
+const nestedTarget = () => document.querySelector( '#nested .target' )
+
+const increaseRankBy = ( n ) => {
+  let x = document.querySelectorAll( '.ranked-list li' )
+  for( let i = 0; i < x.length; i++ )
+    x[i].innerHTML = parseInt( x[i].innerHTML ) + n
+}
+
+const deepestChild = () => {
+  var x = document.querySelector( '#grand-node')
+  console.log( `node: ${x.nodeType}` )
+  var goDeep = node => {
+    if( node.firstElementChild )
+      goDeep( node.firstElementChild )
+    else
+      return node
+  }
+  return goDeep( x )
+}
