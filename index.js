33c5a9ebcd7c7522714c9c48f43f9850d90bc79c
Justin Davis <justin_w_davis@yahoo.com>
2018-10-29 05:25:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const getFirstSelector = selector => document.querySelector( selector )
 
+const nestedTarget = () => document.querySelector( '#nested .target' )
+
+const increaseRankBy = ( n ) => {
+  let x = document.querySelectorAll( '.ranked-list li' )
+  for( let i = 0; i < x.length; i++ )
+    x[i].innerHTML = parseInt( x[i].innerHTML ) + n
+}
+
+const deepestChild = () => {
+  var grand = document.getElementById( 'grand-node' )
+  while ( grand.childElementCount > 0 ){
+      grand = grand.children[0]
+  }
+  return grand
+}
